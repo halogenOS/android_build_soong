9768fb60d648f1fac611344ce0b07d2201f2c5fd
Simão Gomes Viana <devel@superboring.dev>
2023-08-25 11:38:43 +0200

    Allow pigz to be used in PATH
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -102,6 +102,7 @@ var Configuration = map[string]PathConfig{
 	"ld.lld":  Allowed,
 	"llvm-ar": Allowed,
 	"openssl": Allowed,
+	"pigz":    Allowed,
 	"pstree":  Allowed,
 	"rsync":   Allowed,
 	"sh":      Allowed,
