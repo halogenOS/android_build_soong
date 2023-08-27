7bde8a6d0085ffae73038e10a2d53fc2c6d62564
Simão Gomes Viana <devel@superboring.dev>
2023-08-27 18:18:49 +0200

    Use clang r487747c globally

--- cc/config/global.go
+++ cc/config/global.go
@@ -286,8 +286,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r450784d"
-	ClangDefaultShortVersion = "14.0.6"
+	ClangDefaultVersion      = "clang-r487747c"
+	ClangDefaultShortVersion = "17.0.2"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
