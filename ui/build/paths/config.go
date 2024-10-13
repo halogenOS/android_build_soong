15a2a9618e38fb62a8929af9bab2efaab1c55def
Simão Gomes Viana <devel@superboring.dev>
2024-10-13 11:38:42 +0200

    ui: allow perl in PATH
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -103,6 +103,7 @@ var Configuration = map[string]PathConfig{
 	"llvm-ar":     Allowed,
 	"lsof":        Allowed,
 	"openssl":     Allowed,
+	"perl":        Allowed,
 	"pigz":        Allowed,
 	"pstree":      Allowed,
 	"rsync":       Allowed,
