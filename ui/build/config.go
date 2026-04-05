59cac96abade149972a1399ec95a0712e3e95f3e
Simão Gomes Viana <devel@superboring.dev>
2026-04-05 22:48:59 +0200

    soong: Use NumCPU as default parallel job count
    Remove the +2 offset from the default -j value. The extra jobs cause
    unnecessary context switching overhead that slows down builds.

--- ui/build/config.go
+++ ui/build/config.go
@@ -276,8 +276,7 @@ func newConfig(ctx Context, isDumpVar bool, args ...string) Config {
 		ret.skipSoongTests = true
 	}
 
-	// Default matching ninja
-	ret.parallel = runtime.NumCPU() + 2
+	ret.parallel = runtime.NumCPU()
 	ret.keepGoing = 1
 
 	ret.totalRAM = detectTotalRAM(ctx)
